b67cf5a955aba0a1de51ab7586edac6d1c24c507
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 10:09:40 +0200

    +1

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,8 +5,8 @@ The function
 
 
 def add(a: float, b: float) -> float:
-"""
-type-annotated function add that takes a float a and a float b as arguments
-and returns their sum as a float.
-"""
+    """
+    type-annotated function add that takes a float a and a float b as arguments
+    and returns their sum as a float.
+    """
     return (a + b)
