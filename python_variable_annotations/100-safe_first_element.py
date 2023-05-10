4e584244aae59cc20650b90e7396845a073531ad
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 11:46:00 +0200

    +100

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''
+main function
 '''
 from typing import Sequence, Union, Any
 
@@ -7,6 +8,7 @@ from typing import Sequence, Union, Any
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     '''
+    function to document
     '''
     if lst:
         return lst[0]
