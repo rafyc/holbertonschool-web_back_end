82ddc22938fb3b40eb1b2bac485479a2f0072f0b
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 17:37:34 +0200

    +2

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''main function
+'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    '''
+    return asyncio task
+    '''
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
