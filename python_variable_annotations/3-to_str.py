8210acf37793ab327dd6f12dbd3f336b0f6c4b55
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 10:23:39 +0200

    +3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+main function
+"""
+
+
+def to_str(n: float) -> str:
+    '''
+    Function that takes a float n as argument and returns the string
+    representation of the float.
+    '''
+    return str(n)
