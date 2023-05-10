6cd604641ab5d0325dd4f795c491639e0f3bd6b9
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 11:59:32 +0200

    +8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,10 +6,12 @@ from collections.abc import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    '''function that return a callback
+    '''
+    function that return a callback
     '''
     def callback(number: int) -> float:
-        '''the callback function
+        '''
+        the callback function
         '''
         return (number * multiplier)
     return callback
