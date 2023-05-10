e738deeabf77ae49aca4bd707870b525fdd0e718
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 17:51:34 +0200

    +4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''main function
+'''
+from typing import List
+from asyncio import as_completed, create_task
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''should return the list of all the delays (float values).
+    '''
+    arr = []
+    for i in range(n):
+        num = create_task(task_wait_random(max_delay))
+        arr.append(num)
+    sorted = []
+    for j in as_completed(arr):
+        sorted.append(await j)
+
+    return(sorted)
