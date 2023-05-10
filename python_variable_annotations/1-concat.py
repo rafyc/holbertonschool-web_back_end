be6f1a09313fbfbe5d724014c105207b431e888a
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 10:13:10 +0200

    +1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+The function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function concat that takes a string str1 and a string str2 as arguments
+    and returns a concatenated string
+    """
+    return (str1 + str2)
