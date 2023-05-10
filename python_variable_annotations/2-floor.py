a396d16498a5084cfe01b252e6b02a81fdf95a85
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-10 10:19:50 +0200

    +1

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+main function
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    function which takes a float n as argument and returns the floor
+    '''
+    return int(n)
