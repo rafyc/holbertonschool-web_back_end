3736acfe6b8078cb43143832e65f311ad913dc9f
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-11 13:52:21 +0200

    +1

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import random
+import asyncio
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    for i in range (10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
