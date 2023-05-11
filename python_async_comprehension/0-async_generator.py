debb541e679c52fa5cfbc0f79ebb338c0f29bebc
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-11 13:56:52 +0200

    +1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,7 +4,12 @@ import random
 import asyncio
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
-    for i in range (10):
+    '''
+    coroutine will loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number
+    '''
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
