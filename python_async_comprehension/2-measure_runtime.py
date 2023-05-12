da8f9a4258990668078e5c7bd5a4ce7b7160572e
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-12 11:27:02 +0200

    +3

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''main function
+'''
+
+import time
+import asyncio
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measure runtime
+    """
+    sart_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+    end_time = time.time()
+    read_time = end_time - sart_time
+    return read_time
