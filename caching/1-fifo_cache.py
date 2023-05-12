65031e7f8f2400936da43821b6f7dacb2aa52046
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-12 14:42:12 +0200

    +2

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''FIFOCache module
+'''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''
+    put method & get method
+    '''
+    def put(self, key, item):
+        '''put an item in cache and delet the first if full
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            fi = next(iter(self.cache_data))
+            self.cache_data.pop(fi)
+            print(f"DISCARD:{fi}\n")
+
+    def get(self, key):
+        '''Get an item in the cache"
+        '''
+        if key in self.cache_data and key is not None:
+            return self.cache_data[key]
+        return None
