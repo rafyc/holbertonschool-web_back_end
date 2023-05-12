dee6e933cfe23ed96b54d779609781ced57939b5
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-12 14:52:10 +0200

    +3

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''LIFOCache module
+'''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+ '''
+    put method & get method
+    '''
+    def put(self, key, item):
+        '''put an item in cache and delet the first if fullp
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            while True
+                try :
+                   li = next(iter(self.cache_data))
+                self.cache_data.pop(li)
+                print(f"DISCARD: {li}")
+
+    def get(self, key):
+        '''Get an item in the cache"
+        '''
+        if key in self.cache_data and key is not None:
+            return self.cache_data[key]
+        return None
