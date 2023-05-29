e7c54c324078735ff1509c39ab71b23dbb5f4dbf
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-29 16:54:22 +0200

    +2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,27 @@ Main file
 """
 import re
 from typing import List, Tuple
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        formated = filter_datum(self.fields,
+                                RedactingFormatter.REDACTION,
+                                super().format(record),
+                                      RedactingFormatter.SEPARATOR)
+        return formated
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
