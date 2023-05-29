53a72b77d7f2812657b0ef26f59d68d44f98c405
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-29 14:21:42 +0200

    +3

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import re
+from typing import List, Tuple
+
+def filter_datum(fields: List[str], redaction: str, message:str, separator:str) -> str:
+    pattern = r"password=([^;\s]+).*date_of_birth=([^;\s]+)"
+
+    result = re.sub(pattern, f"password={redaction};date_of_birth={redaction}", message)
+    return result
