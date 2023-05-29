8a67138e4d97d73ac909e65ae6163d325e4e343c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-29 22:54:12 +0200

    +2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -28,14 +28,12 @@ class RedactingFormatter(logging.Formatter):
                                 RedactingFormatter.SEPARATOR)
         return formated
 
-    def get_logger()
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Return the log message obfuscated by 'redaction' argument
-    """
-    for i in fields:
-        message = re.sub(i + '=.+?' + separator,
-                         i + '=' + redaction + separator,
-                         message)
-    return message
+    def filter_datum(fields: List[str], redaction: str,
+                     message: str, separator: str) -> str:
+        """Return the log message obfuscated by 'redaction' argument
+        """
+        for i in fields:
+            message = re.sub(i + '=.+?' + separator,
+                             i + '=' + redaction + separator,
+                             message)
+        return message
