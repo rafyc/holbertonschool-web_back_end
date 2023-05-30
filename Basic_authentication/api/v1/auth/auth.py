419c7323c1508f17ae807c9abc2f085312f88a38
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-30 17:03:50 +0200

    +3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,10 +7,21 @@ from typing import List, TypeVar
 class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+         """ Checks if path require authentication
+        :param path: the route check
+        :param excluded_paths: list of route
+        :return: True or False if the route check require authentication
+         """
         return path
 
     def authorization_header(self, request=None) -> str:
+        """ Method return the value of the header request
+        :param request: the route requested
+        :return: None or the header of the request
+        """
         return request
 
     def current_user(self, request=None) -> TypeVar('User'):
+        '''The current user
+        '''
         return request
