3f6303c0f8fd0759ae2822fd891a74902dee4e7a
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-30 11:51:16 +0200

    +6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -12,3 +12,13 @@ def hash_password(password: str) -> bytes:
     pwd = password.encode()
     hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''check password
+    '''
+    pwd = password.encode()
+    if bcrypt.checkpw(pwd, hashed_password):
+        return True
+    else:
+        return False
