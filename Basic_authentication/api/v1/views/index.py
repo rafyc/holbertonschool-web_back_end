381e8ef83c014526055db84232c6ba750eb97491
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-30 16:34:14 +0200

    +1

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def error_401() -> str:
+    """ GET /api/v1/unauthorized
+    Return: error
+    """
+    abort(401)
