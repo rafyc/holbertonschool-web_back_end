76fb356cb2ca845ccfda0db966462abcb202155c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-01 09:18:47 +0200

    +4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -32,14 +32,14 @@ class Auth:
         :param request: the route requested
         :return: None or the header of the request
         """
-        if request is None or 'Authorization' in response:
+        if request is None:
             return None
-        # return request.headers.get('Authorization', None)
-        response = requests.get(request)
-        headers = response.headers.get('Authorization')
-        if headers is None:
-            return None
-        return headers
+        return request.headers.get('Authorization', None)
+        # response = requests.get(request)
+        # headers = response.headers.get('Authorization')
+        # if headers is None:
+        #     return None
+        # return headers
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''The current user
