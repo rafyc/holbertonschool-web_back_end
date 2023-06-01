19f74e7d6e109f248badc4c549e6d5e97f928505
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-01 10:34:33 +0200

    +5

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,7 +9,7 @@ class BasicAuth(Auth):
     """
 
     def extract_base64_authorization_header(self,
-                                            authorization_header: str)-> str:
+                                            authorization_header: str) -> str:
         '''returns the Base64 part of the Authorization header for a
         Basic Authentication:
         '''
