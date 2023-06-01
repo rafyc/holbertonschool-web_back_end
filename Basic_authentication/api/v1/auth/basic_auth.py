a6e92c99cf0a4087e135aa3c0c3e730e624ca419
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-01 10:41:59 +0200

    +6

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Main 0
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,20 @@ class BasicAuth(Auth):
             return None
         value = authorization_header[len('Basic '):]
         return value
+
+    def decode_base64_authorization_header(
+                                           self, base64_authorization_header:
+                                           str) -> str:
+        ''' returns the decoded value of a Base64 string
+        base64_authorization_header:
+        '''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
