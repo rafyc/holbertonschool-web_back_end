0925e0bfe14195ea9af6b6926fd8c1eff523c719
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-01 11:10:46 +0200

    +6

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,3 +39,19 @@ class BasicAuth(Auth):
             return decoded_string
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+                                 self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        '''
+        returns the user email and password from the Base64 decoded value.
+        '''
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        string = decoded_base64_authorization_header.split(":")
+        return(string[0], string[1])
