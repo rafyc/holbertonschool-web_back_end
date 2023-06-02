11122b693c0cb22ea0172359c73963751d76611b
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-02 12:29:05 +0200

    +7

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -43,12 +43,12 @@ class SessionAuth(Auth):
             return False
 
         session_id = self.session_cookie(request)
-            if session_id is None:
-                return False
+        if session_id is None:
+            return False
 
         user_id = self.user_id_for_session_id(session_id)
-            if not user_id:
-                return False
+        if not user_id:
+            return False
 
         del self.user_id_by_session_id[session_id]
             return True
