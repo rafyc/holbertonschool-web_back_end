e6c09d0547d9025facc61ed1a704f44407bdbba5
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-02 12:20:29 +0200

    +7

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -37,3 +37,18 @@ class SessionAuth(Auth):
         cookie = self.session_cookie(request)
         user_id = self.user_id_for_session_id(cookie)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+            if session_id is None:
+                return False
+
+        user_id = self.user_id_for_session_id(session_id)
+            if not user_id:
+                return False
+
+        del self.user_id_by_session_id[session_id]
+            return True
