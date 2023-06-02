9a2f8ab07efd16d7537e67358bb2d4c41c0aa699
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-02 16:31:26 +0200

    +1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,7 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
-from typing import String
 
 
 class DB:
