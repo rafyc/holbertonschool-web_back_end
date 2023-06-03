b56a80d05c65a571767fc5955811605dd37ed20c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-03 14:30:30 +0200

    +8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -45,7 +45,7 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid():
+    def _generate_uuid(self):
         '''generate uuid
         '''
         return str(uuid4())
