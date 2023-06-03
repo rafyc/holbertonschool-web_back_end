0e588ae617343c810898d24d8f10e56831998e33
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-03 12:46:59 +0200

    +3

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 '''Auth module
 '''
-
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -15,3 +17,22 @@ def _hash_password(password: str) -> bytes:
         return hashed
     else:
         pass
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(user.email))
+        except NoResultFound:
+            pswd = _hash_password(password=password)
+            new_user = self._db.add_user(email, pswd)
+            return new_user
