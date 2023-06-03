41075cc28f3cc8db2bf89738175d236ba90793aa
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-03 13:22:37 +0200

    +6

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,8 +3,12 @@
 '''
 
 from flask import Flask, jsonify
+from flask import request
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
@@ -12,6 +16,17 @@ def hello_world():
     '''Main
     '''
     return jsonify(message='Bienvenue')
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email=email, password=password)
+        return jsonify(email=email, message='user created')
+    except ValueError:
+        return jsonify(message='email already registered'), 400
 
 
 if __name__ == "__main__":
