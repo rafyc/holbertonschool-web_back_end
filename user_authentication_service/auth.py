dad96019798c0040fb1be67a9e16b63d6720ab9c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-04 12:21:06 +0200

    +16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -89,11 +89,13 @@ class Auth:
             return None
 
     def get_reset_password_token(self, email: str) -> str:
+        '''Reset password
+        '''
         if email is None:
             return None
         try:
-            user = self._db.find_user_by(email=email)
-            id = _generate_uuid()
+            user: User = self._db.find_user_by(email=email)
+            id: str= _generate_uuid()
             self._db.update_user(user.id, reset_token=id)
             return id
         except Exception:
