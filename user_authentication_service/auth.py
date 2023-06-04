01c572d52fbac2b823cb2403767a2b9cf203341e
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-04 09:24:34 +0200

    +11

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,3 +76,12 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id):
+        '''
+        '''
+        try:
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except NoResultFound:
+            return None
