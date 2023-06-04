25553f13dbe12c57c1b85edb05e1f6b65849ecc8
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-04 10:34:25 +0200

    +14

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -88,10 +88,11 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         if email is None:
-            return None
+            raise ValueError
         try:
             user = self._db.find_user_by(email=email)
             id = _generate_uuid()
             self._db.update_user(user.id, reset_token=id)
+            return id
         except ValueError:
             raise ValueError
