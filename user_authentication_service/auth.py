15db5279bad64204c9026f28caad0c0a4a4a0f54
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-04 10:25:38 +0200

    +14

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -85,3 +85,11 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        try:
+            user = self._db.find_user_by(email=email)
+            id = _generate_uuid()
+            self._db.update_user(user.id, email=email)
+        except ValueError:
+            return None
