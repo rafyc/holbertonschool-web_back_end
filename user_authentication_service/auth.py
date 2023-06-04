779ed1a230364773765c55c96da5d2805aa5ae4b
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-04 11:54:35 +0200

    +16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -102,8 +102,8 @@ class Auth:
         '''
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            pswd = _hash_password(password=password)
+            pswd = _hash_password(password)
             self._db.update_user(user.id, hashed_password=pswd
                                  reset_token=None)
-        except ValueError:
-            return None
+        except Exception:
+            raise ValueError
