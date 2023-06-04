5f524fca3aa203888a52a501777d7090557038f2
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-04 11:59:03 +0200

    +16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -98,11 +98,11 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        '''
+        '''Update password
         '''
         try:
-            user = self._db.find_user_by(reset_token=reset_token)
-            pswd = _hash_password(password)
+            user: User = self._db.find_user_by(reset_token=reset_token)
+            pswd: str = _hash_password(password)
             self._db.update_user(user.id, hashed_password=pswd
                                  reset_token=None)
         except Exception:
