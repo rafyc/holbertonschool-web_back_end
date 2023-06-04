fd6da667afeabd0b713b18aebfc695c81305ba32
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-04 12:19:19 +0200

    +16

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -45,8 +45,10 @@ class Auth:
             return new_user
 
     def valid_login(self, email: str, password: str) -> bool:
+        '''Valid Login
+        '''
         try:
-            user = self._db.find_user_by(email=email)
+            user: User = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode('utf-8'),
                                   user.hashed_password)
         except NoResultFound:
