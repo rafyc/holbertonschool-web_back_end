86aa66042ce1088c47e68061b39421d43fdce1fd
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-05 10:24:37 +0200

    +0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -10,13 +10,11 @@ from typing import Mapping, Sequence, Any
 class TestAccessNestedMap(unittest.TestCase):
     '''
     '''
-    test_cases = [
-    {"nested_map": {"a": 1}, "path": ("a",), "except_rez": 1},
-    {"nested_map": {"a": {"b": 2}}, "path": ("a",), "except_rez": {"b": 2}},
-    {"nested_map": {"a": {"b": 2}}, "path": ("a", "b"), "except_rez": 2}
-    ]
-
-    @parameterized.expand(test_cases)
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
 
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence,
