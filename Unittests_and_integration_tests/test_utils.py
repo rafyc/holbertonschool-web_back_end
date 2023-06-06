67387350180cd73d58669565e28d25afa16e35f9
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-06 13:58:25 +0200

    +2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -16,7 +16,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence,
                                except_rez: Any) -> None:
@@ -29,26 +28,29 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map: Mapping,
-                               path: Sequence,) -> None:
+                                         path: Sequence,) -> None:
         with self.assertRaises(KeyError):
             access_nested_map(nested_map=nested_map, path=path)
+
 
 class TestGetJson(unittest.TestCase):
     '''
     '''
     @parameterized.expand([
         ('http://example.com', {"payload": True}),
-        ('http://holberton.io',{"payload": False})
+        ('http://holberton.io', {"payload": False})
     ])
     @patch('requests.get')
-    def test_get_json(self, test_url : str, expected_payload, mock_get) -> None:
+    def test_get_json(self, test_url: str,
+                      expected_payload, mock_get) -> None:
 
         mock_json = Mock(return_value=expected_payload)
         mock_get.return_value = Mock(json=mock_json)
 
-        rez = get_json(url = test_url)
+        rez = get_json(url=test_url)
 
         self.assertEqual(rez, expected_payload)
+
 
 class TestMemoize(unittest.TestCase):
     '''
