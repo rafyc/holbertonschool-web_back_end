2e24dc1571d668308eae5afbfb65b0740c886eb8
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-07 09:13:10 +0200

    +1

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -2,6 +2,7 @@
 '''Main app
 '''
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
 
@@ -11,3 +12,12 @@ def index():
     '''Generate template
     '''
     return render_template('0-index.html')
+
+babel = Babel(app)
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
