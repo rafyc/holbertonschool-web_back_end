422ef14538b0f1fb6d8d1a71c234ea4fc999478b
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-07 09:01:03 +0200

    +0

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+'''Main app
+'''
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''Generate template
+    '''
     return render_template('index.html')
 
 if __name__ == '__main__':
