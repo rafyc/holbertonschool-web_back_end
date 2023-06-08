8fdf88091171088d55d56764a902221294182441
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-08 10:03:50 +0200

    +2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -26,8 +26,10 @@ def index():
     return render_template('2-index.html')
 
 
-@babel.localeselector
 def get_locale():
     '''
     '''
     return request.accept_languages.best_match('LANGUAGES')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8001")
