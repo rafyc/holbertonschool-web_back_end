62d3b09dabc01cb083075a69de6769f129f5809c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-08 14:08:28 +0200

    +4

---
+++ i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''Main app
+'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''Configuration module
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    '''Generate template
+    '''
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    '''
+    '''
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match('LANGUAGES')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8001")
