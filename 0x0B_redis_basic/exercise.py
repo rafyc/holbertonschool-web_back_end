ee47d32a6797d2ee21e193dd70f9ea8ed6f077ed
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-06-26 10:17:31 +0200

    +2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,7 +20,7 @@ class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
