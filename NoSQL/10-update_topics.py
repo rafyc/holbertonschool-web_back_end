79a05b681bbc5d69e53461c12e55dae1b740be38
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-03 11:00:43 +0200

    +10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,6 +6,5 @@ from pymongo import MongoClient
 def update_topics(mongo_collection, name, topics):
     """ Change school topics """
     query = {"name": name}
-    update = {$set:{"topics":topics}}
-    documents = mongo_collection.update(query, update)
-    return documents
+    update = {"$set":{"topics":topics}}
+    mongo_collection.update(query, update)
