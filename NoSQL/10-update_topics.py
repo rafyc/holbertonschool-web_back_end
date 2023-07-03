11e617b78acf1cac536e67f1db0f138199af757b
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-03 11:03:28 +0200

    +10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,4 +7,4 @@ def update_topics(mongo_collection, name, topics):
     """ Change school topics """
     query = {"name": name}
     update = {"$set":{"topics":topics}}
-    mongo_collection.updateMany(query, update)
+    mongo_collection.update_many(query, update)
