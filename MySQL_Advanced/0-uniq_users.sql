e31d2ca2ecf84101ea34c1d022562de1424bcc8a
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-04 18:03:44 +0200

    +0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- script that creates a table users
-CREATE TABLE users IF NOT EXISTS (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY(id),
-    email CHAR(255) NOT NULL UNIQUE,
-    name CHAR(255)
-    ;)
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+    );
