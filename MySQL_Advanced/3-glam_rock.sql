97dc9840791003028fae10e09cab73d0cec72809
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-07 10:03:35 +0200

    +3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,5 @@
---- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-
-SELECT band_name,
-IF(split IS NULL, YEAR(CURDATE()) - formed, split - formed) AS lifespan
+-- script that lists all bands with Glam rock as their main style
+SELECT band_name, COALESCE(split, YEAR(NOW())) - formed AS lifespan
 FROM metal_bands
-WHERE style LIKE '^Glam rock'
+WHERE BINARY style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
