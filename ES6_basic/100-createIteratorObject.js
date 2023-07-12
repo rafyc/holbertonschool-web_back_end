22c38caee6d8d30c6acf3f429f5b03a3a89c41ef
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-12 11:54:28 +0200

    +100

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,7 @@
 export default function createIteratorObject(report) {
   const arr = [];
-  for (const name of Object.values(report.allEmployees))
+  for (const name of Object.values(report.allEmployees)) {
     arr.push(...name);
+  }
   return arr;
 }
