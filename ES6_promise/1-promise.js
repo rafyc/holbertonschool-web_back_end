af0c75c43804cfb55849a47e994a2b058ef2f162
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-12 16:26:03 +0200

    0

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,11 @@
 function getFullResponseFromAPI(success) {
-  new Promise((success) => {
-    if (success)
-      return {
-        status: 200,
-        body: 'succes'
-      }
-  })
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
+
+export default getFullResponseFromAPI;
