4e364a03e4eef4278ed9ab7f2f844c712c8c9f35
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-12 17:19:37 +0200

    34

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
-import { uploadPhoto, createUser } from utils
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return new Promise.all([uploadPhoto(), createUser()]).then((val) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((val) => {
     console.log(`${val[0].body} ${val[1].firstName} ${val[1].lastName}`);
   })
     .catch(() => console.log('Signup system offline'));
