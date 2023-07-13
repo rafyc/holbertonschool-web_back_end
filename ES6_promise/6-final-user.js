9716927e3fc4833dbb26c68cfa1eea66b512d587
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-13 10:19:44 +0200

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,11 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  const p1 = signUpUser(firstName, lastName)
-  const p2 = uploadPhoto(fileName)
-  return Promise.allSettled([p1, p2]).then((values) => {
+export default /*async */function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser();
+  const photo = uploadPhoto();
+
+  return Promise.allSettled([user, photo]).then((values) => {
     values.map((v) => ({ status: v.status, value: v.status === 'fulfilled' ? v.value : `${v.reason.name}: ${v.reason.message}` }))
   });
 }
-
-export default handleProfileSignup
