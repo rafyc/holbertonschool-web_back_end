d63af16fc94971d8005fc3551092538ccdee42d9
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-17 12:58:49 +0200

    2

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 const getListStudentIds = (array) => {
   if (!Array.isArray(array))
-    return []
-  return array.map(item => item.id)
+    return [];
+  return array.map(item => item.id);
 }
 
 export default getListStudentIds;
