19266fe5d344ac597eb40118550133bc4ab0580e
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-17 10:56:52 +0200

    1

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -3,24 +3,24 @@ const getListStudents = () => {
   let students = []
 
   let student = {}
-  student.firstName = "Guillaume"
   student.id = 1
+  student.firstName = "Guillaume"
   student.location = "San Francisco"
   students.push(student)
 
   let student1 = {}
-  student1.firstName = "James"
   student1.id = 2
+  student1.firstName = "James"
   student1.location = "Columbia"
   students.push(student1)
 
   let student2 = {}
-  student2.firstName = "Serena"
   student2.id = 2
+  student2.firstName = "Serena"
   student2.location = "San Francisco"
   students.push(student2)
 
-  return students
+  return students;
 
 }
 
---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,5 @@
+const getListStudentIds = () => {
+
+}
+
+export default getListStudentIds;
