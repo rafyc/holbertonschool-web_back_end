7ca0a617194f209616e9ab44ac9c7b6d0eeb5dd0
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-17 16:52:14 +0200

    +4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,8 @@
-const updateStudentGradeByCity = (students, city, newGrades) => {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((item) => item.location === city)
     .map((item) => {
-      const obj = newGrades.find(grade => grade.studentId === item.id)
+      const obj = newGrades.find((grade) => grade.studentId === item.id);
       return { ...item, grade: obj ? obj.grade : 'N/A' };
     });
 };
-
-export default updateStudentGradeByCity;
