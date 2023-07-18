ba38e2395d0e5416170f3a2009c1b5ef4c6e75b5
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-18 15:26:24 +0200

    +8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
   const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
-  const cleanedValues = filteredValues.map(value => value.slice(startString.length));
+  const cleanedValues = filteredValues.map(value => value.substring(startString.length));
   return cleanedValues.join('-');
 }
