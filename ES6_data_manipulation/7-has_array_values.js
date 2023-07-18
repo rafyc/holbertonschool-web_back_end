2d7f58b78a4d592cb72a685e9c636dd89adecda1
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-18 14:34:16 +0200

    +7

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  if (array.map(x => set.has(x)).every(Boolean))
-    return true;
-  return false;
+  return array.every((x) => set.has(x));
 };
