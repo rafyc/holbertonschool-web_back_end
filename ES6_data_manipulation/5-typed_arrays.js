fb90bbc412d88dfea1d701dd17a2ddf4b02f6794
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-18 12:53:16 +0200

    +5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
