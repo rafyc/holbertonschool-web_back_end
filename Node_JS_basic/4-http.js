9d05790bfc29240761cce2d0e684d8fcebf2c31e
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-21 11:26:49 +0200

    +4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,8 @@
 const http = require('node:http');
 
-// Create a local server to receive data from
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
+module.exports = app;
