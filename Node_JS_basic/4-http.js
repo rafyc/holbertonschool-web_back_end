70dc0245d462bd45017a60a8fbb208ba064f65f9
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-21 11:28:41 +0200

    +4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,6 +1,8 @@
 const http = require('node:http');
 
 const app = http.createServer((req, res) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
