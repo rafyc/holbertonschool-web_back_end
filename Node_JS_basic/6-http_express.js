c55d2d608210e13a545194bc502901af0e81c402
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-21 15:22:49 +0200

    +6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const hostname = '127.0.0.1';
 const port = 1245
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!')
 })
 
-app.listen(port)
+app.listen(port, hostname)
