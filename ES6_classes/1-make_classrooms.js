de264f6843f98246f53eed61f7d843e93dbc3af1
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-22 15:08:17 +0200

    2

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
 import ClassRoom from './0-classroom';
 
-const initializeRooms = () => {
+export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
   ];
 };
-
-export default initializeRooms;
