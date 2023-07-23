428e13227dca57e7cae8dab0d41ee93e28f2f4c0
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-23 15:14:45 +0200

    3

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,9 +1,13 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this.sqft = sqft;
   }
 
-  get() {
+  get sqft() {
     return this._sqft;
   }
 
@@ -12,5 +16,9 @@ export default class Building {
       throw new Error('sqft must be a number')
     }
     this._sqft = newSqtf
+  }
+
+  evacuationWarningMessage() {
+
   }
 }
