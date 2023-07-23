ba622570dd812695d0bb22d611e1e9dee7b5c651
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-23 14:36:48 +0200

    2

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set(newAmount) {
+    if (typeof newAmount != 'number') {
+      throw new Error('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} ${this.currency.code}`
+  }
+
+  convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' && typeof conversionRate !== 'number') {
+      throw new Error('Amout must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
