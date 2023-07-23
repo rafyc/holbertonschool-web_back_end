222be8a03b5d9fd5e644527174ed7fe229596f69
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-23 14:53:54 +0200

    3

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -26,7 +26,7 @@ export default class Pricing {
 
   displayFullPrice() {
     return `${this.amount} ${this.currency.name} (${this.currency.code})`
-  }
+  };
 
   static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number' && typeof conversionRate !== 'number') {
