04a21cce491ba33c88698c8bce0e134a6990b5bf
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-26 14:05:22 +0200

    2

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,16 +1,18 @@
 const calculateNumber = (type, a, b) => {
-  if (type.includes('SUM')) {
-    return Math.round(a) + Math.round(b);
-  } else if (type.includes('SUBTRACT')) {
-    return Math.round(a) - Math.round(b);
-  } else if (type.includes('DIVIDE')) {
-    if (b === 0) {
-      return ('Error')
-    }
-    return Math.round(a) / Math.round(b);
-  } else {
-    return 'Error';
+
+  const arg1 = Math.round(a);
+  const arg2 = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return arg1 + arg2;
+    case 'SUBTRACT':
+      return arg1 - arg2;
+    case 'DIVIDE':
+      return (arg2 === 0) ? 'Error' : arg1 / arg2;
+    default:
+      throw new Error('The type parameter only accept SUM, SUBTRACT, or DIVIDE');
   }
-};
+}
 
 module.exports = calculateNumber;
