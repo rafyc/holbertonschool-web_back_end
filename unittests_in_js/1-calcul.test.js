127e5c85017e185446078ecfc84c39a76fdc6bf2
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-26 13:19:53 +0200

    2

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -2,14 +2,17 @@ var assert = require('assert');
 const calculateNumber = require("./1-calcul.js");
 describe('SUM', function () {
   it('should round and add', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
   })
 });
 describe('SUBTRACT', function () {
   it('should round and subtract', function () {
+    assert.strictEqual(calculateNumber(1, 3), -2);
   })
 });
 describe('DIVIDE', function () {
   it('should round and divid', function () {
+    assert.strictEqual(calculateNumber(1, 3), 0.3333333333333333);
   })
 });
 
