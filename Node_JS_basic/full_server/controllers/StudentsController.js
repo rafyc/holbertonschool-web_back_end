8fda5bc2192fdda2ae65a7071899e7a8e6598d34
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-26 09:48:42 +0200

    last

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -32,6 +32,7 @@ class StudentsController {
       .catch((err) => {
         console.error(err);
         response.status(500).send('Cannot load the database'); // Send an error response
+        return;
       });
   }
 }
