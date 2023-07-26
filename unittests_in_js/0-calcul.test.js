0450b17054eef83b5e88c084428ca062f0fc6517
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-07-26 12:53:41 +0200

    1

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,8 +3,21 @@ const calculateNumber = require("./0-calcul.js");
 describe('calculateNumber', function () {
   it('should round and add', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1.6, 1.8), 4)
+    assert.strictEqual(calculateNumber(1.6, 1.8), 4);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
     assert.strictEqual(calculateNumber(1, 3.9), 5);
-    assert.strictEqual(isNaN(calculateNumber(1,)), true)
+    assert.strictEqual(isNaN(calculateNumber(1,)), true);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
 });
